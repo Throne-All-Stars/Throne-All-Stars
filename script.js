33170c9db1b07f47eead30b9edc7ff45167defdc
petthecat19 <adrianpena365@gmail.com>
2023-08-31 20:55:54 +0000

    added images with full names

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-
-
 const charBio = "https://thronesapi.com/api/v2/Characters"
 
 function fetchCharacters() {
     fetch(charBio)
         .then((res) => res.json())
         .then((data) => {
-            // const charFullName = (data.id.fullName)
-            // console.log(data[32].fullName)
-            
-            
-            const character = data[1].fullName;
-            const ul = document.getElementById("Remainder")
-            
-            data.forEach(character => {
-                const favs = document.createElement('li');
-                const anchor = document.createElement('a');
-                anchor.href = character.url;
-                anchor.textContent = character.fullName;
-                console.log(character.fullName)
+            console.log(data)
 
-                favs.appendChild(anchor);
-                ul.appendChild(favs);
+
+            const character = data.fullName;
+            const ul = document.getElementById("Remainder")
+            const elements = data
+
+            // console.log("images", images)
+
+            elements.forEach(element => {
+                console.log(element.imageUrl);
+
+                const div = document.createElement("div");
+                const img = document.createElement("img");
+                const name = document.createElement("p");
+
+                console.log("full name:", element.fullName);
+
+                img.src = element.imageUrl;
+                name.textContent = element.fullName; // Corrected line
+
+                div.appendChild(img);
+                div.appendChild(name); // Append the name element
+                document.body.append(div);
             });
+
+
+            // data.forEach(character => {
+            //     const favs = document.createElement('li');
+            //     const anchor = document.createElement('a');
+            //     // anchor.href = character.url;
+            //     anchor.textContent = character.fullName;
+            //     console.log(character.fullName)
+
+            //     favs.appendChild(anchor);
+            //     ul.appendChild(favs);
+            // });
         })
         .catch(error => {
             console.error("Error: ", error);
@@ -31,28 +49,28 @@ function fetchCharacters() {
 
 console.log(fetchCharacters())
 
-function fetchFavorites() {
-    fetch(charBio)
-        .then((res) => res.json())
-        .then((data) => {
-            const ul = document.getElementById("Favorites-list");
+// function fetchFavorites() {
+//     fetch(charBio)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             const ul = document.getElementById("Favorites-list");
 
-            for (let i = 0; i < 5; i++) {
-                const character = data[i];
-                const favs = document.createElement('li');
-                const anchor = document.createElement('a');
-                anchor.href = character.url;
-                anchor.textContent = character.fullName;
-                console.log(character.fullName);
+//             for (let i = 0; i < 5; i++) {
+//                 const character = data[i];
+//                 const favs = document.createElement('li');
+//                 const anchor = document.createElement('a');
+//                 // anchor.href = character.url;
+//                 anchor.textContent = character.fullName;
+//                 // console.log(character.fullName);
 
-                favs.appendChild(anchor);
-                ul.appendChild(favs);
-            }
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-        });
-}
+//                 favs.appendChild(anchor);
+//                 ul.appendChild(favs);
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Error: ", error);
+//         });
+// }
 
 fetchFavorites()
 
