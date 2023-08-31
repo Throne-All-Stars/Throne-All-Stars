68c1c9e1d419950037d1b8a16e264fcbe91be194
petthecat19 <adrianpena365@gmail.com>
2023-08-31 16:39:04 +0000

    Character named rendered to the screen as items of an unordered list.

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const charBio = "https://anapioficeandfire.com/api/characters/583"
+const charBio = "https://thronesapi.com/api/v2/Characters"
 
 function fetchAndDisplay() {
-fetch(charBio)
-.then(res => response.json())
-.then(data => {
-    const minChar = data.results.slice(0,5);
-    const ul = document.getElementById("Name")
-    
-    character.forEach(character => {
-        const favs = document.createElement('Favorites-List');
-        const anchor = document.createElement('anc');
-        anchor.href = character.url;
-        anchor.TextContent = character.name;
-        
-        favs.appendChild(anchor);
-        ul.appendChild(favs);
-    });
-})
-.catch(error => {
-    console.error("Error: ", error);
-});
+    fetch(charBio)
+        .then((res) => res.json())
+        .then((data) => {
+            // const charFullName = (data.id.fullName)
+            console.log(data[32].fullName)
+            
+            const character = data[1].fullName;
+            const ul = document.getElementById("Favorites-list")
+            
+            data.forEach(character => {
+                const favs = document.createElement('li');
+                const anchor = document.createElement('a');
+                anchor.href = character.url;
+                anchor.textContent = character.fullName;
+                console.log(character.fullName)
+
+                favs.appendChild(anchor);
+                ul.appendChild(favs);
+            });
+        })
+        .catch(error => {
+            console.error("Error: ", error);
+        });
 }
+
+console.log(fetchAndDisplay())
